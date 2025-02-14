0d15edbc4014296cbb706a2940d65e0971846020
L1289 <lallsuk@sheridancollege.ca>
2025-02-14 11:57:20 -0500

    Trying to Get targeting to work

---
+++ Assets/Scripts/FSM Action Scripts/Decide_Target_AT.cs
@@ -0,0 +1,61 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class Decide_Target_AT : ActionTask {
+
+        public BBParameter <Vector3> targetPosition;
+		public BBParameter <float> hungerBar;
+        public BBParameter<float> targetDistance;
+
+		public Transform hungerWaypoint;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+
+			float currentHungerBar = hungerBar;
+
+
+            if (currentHungerBar <= 10f)
+			{
+				//The direction the penguin is moving in for hunger
+				Vector3 directionToHungerBar = hungerWaypoint.position - agent.transform.position;
+
+                //How far they are moving
+                Vector3 hungerbarTarget = agent.transform.position + directionToHungerBar.normalized * directionToHungerBar.magnitude;
+
+				//Setting Target to restore hungerbar
+				targetPosition.value = hungerbarTarget;
+
+				targetDistance = hungerbarTarget.x + hungerbarTarget.y + hungerbarTarget.z;
+
+				EndAction(true);
+
+            }
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
