9994388f7e6be450802dcb75c32db90077314c11
L1289 <lallsuk@sheridancollege.ca>
2025-02-14 10:52:43 -0500

    Tried to make fish spawn
    Tried to set up fish spawning above the fish spawn when the player presses space bar however for some reason I can't get it to work need to look into it

---
+++ Assets/Scripts/FSM Scripts/Spawn_Fish_AT.cs
@@ -0,0 +1,55 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+using System.Collections;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+using NodeCanvas.Editor;
+using UnityEditor;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class Spawn_Fish_AT : ActionTask {
+
+		public GameObject fish;
+
+        public Transform fishSpawner;
+
+		private Vector3 fishSpawnerPos;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+            return null;
+		}
+       
+        //This is called once each time the task is enabled.
+        //Call EndAction() to mark the action as finished, either in success or failure.
+        //EndAction can be called from anywhere.
+        protected override void OnExecute() {
+			//EndAction(true);
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+
+			fishSpawnerPos = new Vector3(fishSpawner.position.x, fishSpawner.position.y, fishSpawner.position.z);
+		    if (Input.GetKeyDown("space"))
+			{
+				
+            }
+
+        }
+
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
